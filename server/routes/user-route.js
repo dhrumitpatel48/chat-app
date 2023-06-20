00aca341665b6e3ae3d810273c428424e33ad968
Dhrumit Patel <80308003+dhrumitt@users.noreply.github.com>
2023-06-20 22:49:42 +0530

    Login Functionality completed (#6)

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,6 +1,7 @@
-const { register } = require("../controllers/user-controller");
+const { register, login } = require("../controllers/user-controller");
 
 const router = require("express").Router();
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;

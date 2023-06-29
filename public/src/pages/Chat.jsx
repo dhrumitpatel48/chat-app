5d03dce4ca0d55b9d51c3582940f67af5c6dea6b
Dhrumit Patel <80308003+dhrumitpatel48@users.noreply.github.com>
2023-06-29 21:52:46 +0530

    display all the contacts with styles (#13)

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -9,6 +9,7 @@ function Chat() {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [curretChat, setCurrentChat] = useState(undefined);
 
   useEffect(() => {
     async function getContacts() {
@@ -30,10 +31,13 @@ function Chat() {
     }
     getContacts();
   }, []);
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat)
+  }
   return (
     <Container>
       <div className="container">
-        <Contacts contacts={contacts} currentUser={currentUser} />
+        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange}/>
       </div>
     </Container>
   );

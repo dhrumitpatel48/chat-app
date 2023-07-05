aa1c23eb999174b5f03600df7d178806823cd3ba
Dhrumit Patel <80308003+dhrumitpatel48@users.noreply.github.com>
2023-07-05 22:23:12 +0530

    Design Chat Input Completed (#18)

---
+++ public/src/components/Messages.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Picker from "emoji-picker-react";
+import { IoMdSend } from "react-icons/io";
+import { BsEmojiSmileFill } from "react-icons/bs";
+export default function Messages() {
+  return <Container>Messages</Container>;
+}
+
+const Container = styled.div``;

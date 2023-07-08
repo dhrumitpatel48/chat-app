b6a24a881195969c538ca9f3813ff0179aa3d86a
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-07-08 20:23:10 +0530

    Store and retrieve messages completed

--- server/controllers/message-controller.js
+++ server/controllers/message-controller.js
@@ -1,2 +1,40 @@
-module.exports.addMessage = (req, res, next) => {}
-module.exports.getAllMessage = (req, res, next) => {}
+const messageModel = require("../model/message-model");
+
+module.exports.addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const data = await messageModel.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (!data) return res.json({ msg: "Failed to add message to database" });
+    return res.json({ msg: "Message added successfully." });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllMessage = async (req, res, next) => {
+  try {
+    const { from, to } = req.body;
+    const message = await messageModel
+      .find({
+        users: {
+          $all: [from, to],
+        },
+      })
+      .sort({ updatedAt: 1 });
+
+    const projectMessages = message.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+
+    res.json(projectMessages)
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/model/message-model.js
+++ server/model/message-model.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const messageSchema = new mongoose.Schema(
   {
     message: {
-      type: String,
-      required: true,
+      text: { type: String, required: true },
     },
     users: Array,
     sender: {

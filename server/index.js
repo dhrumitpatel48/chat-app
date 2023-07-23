5d475500721a3ecafd0577f59450b56972340f15
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-07-23 12:29:07 +0530

    Completed integrating sockets

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/user-route");
 const messageRoutes = require("./routes/message-route");
 
 const app = express();
+const socket = require("socket.io");
 require("dotenv").config();
 
 app.use(cors());
@@ -28,4 +29,27 @@ mongoose
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT} successfully.`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => { 
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
 });
